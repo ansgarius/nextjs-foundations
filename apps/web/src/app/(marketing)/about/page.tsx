2fdaa65f1fc574de0c948f72fcb63ecfbcc784c6
sistemasConsufin <sistemas@consufin.com.mx>
2026-09-25 20:36:15 -0600

    t2

--- apps/web/src/app/(marketing)/about/page.tsx
+++ apps/web/src/app/(marketing)/about/page.tsx
@@ -14,9 +14,9 @@ export default async function AboutPage() {
       </p>
       <button
         type="button"
-        onClick={() => {
+        /* onClick={() => {
           throw new Error("Test error boundary");
-        }}
+        }} */
         className="rounded bg-gray-900 px-4 py-2 text-white hover:bg-gray-800"
       >
         Throw Error
